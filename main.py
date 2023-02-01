f0e4d6ca012f126588bc2624efe88c05bbdd2fcc
KEYBODY65 <popov27051971@gmail.com>
2023-02-01 16:50:25 +0300

    Start

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print('Я добавил html')
+from flask import Flask
+
+app = Flask(__name__)  # созжаём объект класса Flask
+
+
+@app.route('/')  # Отслеживание(переход) на главную страницу
+def main():
+    return "Привет, это главная странца"
+
+
+@app.route('/reg')
+def registration():
+    return 'Страница регистарации пользователя'
+
+
+@app.route('/history')
+def history():
+    return 'Показания датчиков теплицы за всё время'
+
+
+if __name__ == '__main__':  # условие запуска локального сервера
+    app.run(debug=True)  # debug стоит временно, он показывается все ошибки на смой странице
