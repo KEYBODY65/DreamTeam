c6d0c952441ff057750b6b055492121c7d952418
user <andreev200624@gmail.com>
2023-02-02 14:15:08 +0300

    ууууууeee

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
-app = Flask(__name__)  # созжаём объект класса Flask
+app = Flask(__name__)  # создаём объект класса Flask
 
 
 @app.route('/')  # Отслеживание(переход) на главную страницу
 @app.route('/home')
 def main():
-    return "Управление теплицей"
+    return render_template('index.html')
 
 
 @app.route('/user')
 def history():
-    return 'Ручное внесение изменений'
+    return render_template('user.html')
 
 
 @app.route('/testimony')
 def testimony():
-    return 'Таблица с показаниями + средняя температура'
+    return render_template('history.html')
 
 
 if __name__ == '__main__':  # условие запуска локального сервера
