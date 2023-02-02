7c71c662769c0782d85389179d0a857845cdc216
user <andreev200624@gmail.com>
2023-02-02 15:17:14 +0300

    OMG

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLalchemy
+import requests
 
 app = Flask(__name__)  # создаём объект класса Flask
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+db = SQLalchemy(app)
+data = requests.get("https://dt.miet.ru/ppo_it/api/temp_hum/<number>")  # кидаем Гет запрос
+
+
+class Indications(db.Model):
+    data_base = {'temperature_sensor': {  # датчик температуры
+        'id': ' device_id',
+        'temperature': ' temp_value',
+        'humidity': 'hum_value'
+    },
+
+        'auto_door': {  # дверца
+            'state': 'state_value'
+        },
+
+        'WATERING MACHINE': {  # поливалка
+            'id': 'device_id',
+            'state': 'state_value'
+        },
+
+        'humidification system': {  # система увлажнения
+            'state': 'state_value'
+        }}
 
 
 @app.route('/')  # Отслеживание(переход) на главную страницу
