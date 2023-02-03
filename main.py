a79053d40c81b3c8e8a320732978f61312ca4fea
KEYBODY65 <popov27051971@gmail.com>
2023-02-03 15:07:40 +0300

    MishaP

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
+from datetime import datetime
+import sqlite3
 import requests
 
 app = Flask(__name__)  # создаём объект класса Flask
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
-db = SQLAlchemy(app)
 data = requests.get("https://dt.miet.ru/ppo_it/api/temp_hum/<number>")  # кидаем Гет запрос
 
 
-class Indications(db.Model):
-    """
-        CREATE TABLE IF NOT EXISTS temperature_sensor (id serial4, temperature int, humidity int);
-        CREATE TABLE IF NOT EXISTS auto_door (state bool);
-        CREATE TABLE IF NOT EXISTS "WATERING MACHINE" (id serial4, state bool);
-        CREATE TABLE IF NOT EXISTS "humidification system" (id serial4, state bool);
-    """
+def make_a_database():
+    with sqlite3.connect("db/databse.db") as db:
+        db.cursor()
+        for_tem_sensor = 'CREATE TABLE IF NOT EXISTS temperature_sensor (id serial4, temperature int, humidity int);'
+        for_door = 'CREATE TABLE IF NOT EXISTS auto_door (state bool);'
+        for_wat_machine = 'CREATE TABLE IF NOT EXISTS "WATERING MACHINE" (id serial4, state bool);'
+        for_humid = 'CREATE TABLE IF NOT EXISTS "humidification system" (id serial4, state bool);'
+        db.execute(for_tem_sensor)
+        db.execute(for_door)
+        db.execute(for_wat_machine)
+        db.execute(for_humid)
+
+def time(year,month, day):
+    return datetime.timestamp(datetime)
+
 
 @app.route('/')  # Отслеживание(переход) на главную страницу
 @app.route('/home')
