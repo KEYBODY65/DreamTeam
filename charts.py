c0ea69586bcb96e7b903424d538a12ade66e541e
user <andreev200624@gmail.com>
2023-02-09 15:07:29 +0300

    upd_4

--- charts.py
+++ charts.py
@@ -1,20 +1,19 @@
-from db.db_ap import Database_API
-import numpy as np
 import matplotlib.pyplot as plt
+from db.db_ap import get_values
+fig2 = plt.figure(figsize=(8, 4))
+ax2 = fig2.add_subplot()
 
-d = Database_API('db/databse.db')
 
-
-def make_chats(time, value, id):  # Значения по у, сами значения столбцов
-    fig2 = plt.figure(figsize=(8, 4))
-    ax2 = fig2.add_subplot()
-
+def make_charts():
     ax2.set_ylabel('Влажность почвы, %', color='mediumturquoise')
     ax2.set_xlabel('Время')
 
-    time = [f'{11 + i}:00' for i in range(6)]
-    value = [d.connect("SELECT * FROM sensor_values", off=False, fetchall=True)]
-    print(value)
+    x2 = [f'{11 + i}:00' for i in range(6)]
+    y2 = []
+    ax2.bar(x2, y2, color='mediumturquoise')
+    ax2.grid()
+    for i in make_charts():
+        y2.append(i['humidification'])
+    plt.title('Просмотр влажности почвы 1-го датчика')
 
-for i in d.connect("SELECT * FROM sensor_values", off=False, fetchall=True):
-    print(f"{i['id_sensor']}, {i['val']}, {i['n_time']}")
+    plt.show()
