ecf4997f2813430c9a80ef54e40139cbef2138a9
KEYBODY65 <popov27051971@gmail.com>
2023-03-17 13:50:13 +0300

    233

--- db/db_ap.py
+++ db/db_ap.py
@@ -48,7 +48,7 @@ class Database_API:
         return data
 
 # d = Database_API('database.db')
-# # # d.create_tables()
+# d.create_tables()
 # for i in range(1, 6):
 #     print(d.get_values(i))
 # d.connect("SELECT * FROM sensor_values", off=False, fetchall=True)
